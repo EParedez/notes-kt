d1c7672a3fa5cf7426878f8249bbb4c869ef3184
Emanuell Paredes <emanuellp@hotmail.com>
2020-04-22 09:09:20 -0500

    connect to cassandra

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # notes-kt
+
+
+###run cassandra
+* docker run --name cas1 -p 9042:9042  -d cassandra
+* to validate the above, connect to port 9042, not user not password
+* run the following sentence CQL
+```
+CREATE KEYSPACE keyspacenotes
+WITH replication = {
+	'class' : 'NetworkTopologyStrategy',
+	'datacenter1' : 1,
+	'datacenter2' : 1
+};
+
+
+CREATE TABLE keyspacenotes.mytable (
+	id int primary key,
+	name text
+);
+```
+
+###references
+* https://www.quora.com/Which-ports-does-Cassandra-use-and-how-are-they-used
+* https://gokhanatil.com/2018/02/build-a-cassandra-cluster-on-docker.html
+* https://www.baeldung.com/spring-data-cassandra-tutorial
+* https://docs.spring.io/spring-data/cassandra/docs/current/reference/html/#reference
+* https://hub.docker.com/_/cassandra
+* https://medium.com/@michaeljpr/five-minute-guide-getting-started-with-cassandra-on-docker-4ef69c710d84
---
+++ src/main/kotlin/com/eparedez/todokotlin/TodoKotlinCassandra.kt
@@ -0,0 +1,60 @@
+package com.eparedez.todokotlin
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean
+import org.springframework.data.cassandra.config.CassandraSessionFactoryBean
+import org.springframework.data.cassandra.config.SchemaAction
+import org.springframework.data.cassandra.core.CassandraOperations
+import org.springframework.data.cassandra.core.CassandraTemplate
+import org.springframework.data.cassandra.core.convert.CassandraConverter
+import org.springframework.data.cassandra.core.convert.MappingCassandraConverter
+import org.springframework.data.cassandra.core.mapping.BasicCassandraMappingContext
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext
+import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories
+
+@Configuration
+@EnableCassandraRepositories()
+class TodoKotlinCassandra {
+
+    companion object {
+        const val KEY_SPACE_NAME = "keyspacenotes"
+    }
+
+    @Bean
+    fun cluster() : CassandraClusterFactoryBean {
+        val cluster = CassandraClusterFactoryBean()
+        cluster.setContactPoints("localhost") //TODO change to .properties
+        cluster.setJmxReportingEnabled(false)
+        return cluster
+    }
+
+    @Bean
+    fun mappingContext() : CassandraMappingContext{
+        val mappingContext = BasicCassandraMappingContext()
+        mappingContext.setUserTypeResolver(SimpleUserTypeResolver(cluster().`object`, KEY_SPACE_NAME)) //TODO change to .properties
+        return mappingContext
+    }
+
+    @Bean
+    fun convert() : CassandraConverter{
+        return MappingCassandraConverter(mappingContext())
+    }
+
+    @Bean
+    fun session(): CassandraSessionFactoryBean{
+        val session = CassandraSessionFactoryBean()
+        session.setCluster(cluster().`object`)
+        session.setKeyspaceName(KEY_SPACE_NAME)
+        session.setConverter(convert())
+        session.schemaAction = SchemaAction.NONE
+        return session
+    }
+
+    @Bean
+    fun cassandraTemplate() : CassandraOperations{
+        return CassandraTemplate(session().`object`)
+    }
+
+}
