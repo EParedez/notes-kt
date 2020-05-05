8954261a4cf1bb782c15b3ffbfc87e6e0c08b1c3
Alexander Miss <mail@alexandermiss.com>
2020-05-05 01:11:55 -0500

    Api Rest Post Action
    Api Rest Post Action 2

--- src/main/kotlin/com/eparedez/todokotlin/api/NoteController.kt
+++ src/main/kotlin/com/eparedez/todokotlin/api/NoteController.kt
@@ -3,9 +3,12 @@ package com.eparedez.todokotlin.api
 import com.eparedez.todokotlin.models.entity.Note
 import com.eparedez.todokotlin.models.repositories.INoteRepository
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType.*
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import java.util.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/notes")
@@ -15,8 +18,14 @@ class NotesController(private val noteRepository: INoteRepository){
     fun findAll(): List<Note> = noteRepository.findAll()
 
     @GetMapping("/{id}")
-    fun findOne(@PathVariable id: UUID) =
-            noteRepository.findById(id) ?:
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "This notes does not exists")
+    fun findOne(@PathVariable id: UUID) : ResponseEntity<Note> {
+        return noteRepository.findById(id).map { note -> ResponseEntity.ok(note) }
+                .orElse(ResponseEntity.notFound().build())
+    }
+
+    @PostMapping(produces = [APPLICATION_JSON_VALUE])
+    fun create(@Valid @RequestBody note: Note) =
+        noteRepository.save(note) ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request")
+
 
 }
